d7f6c15becf2fa80c570a9ff71111e284867387a
mferchaguilar <60389982+mferchaguilar@users.noreply.github.com>
2020-05-18 18:29:19 +0200

    deleting non-useful versions
    deleting non-useful folders

--- aislarte_django/aislarte/aislarteweb/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from aislarteweb import views
-
-urlpatterns = [
-    path('', views.aislarteweb, name='aislarteweb'),
-    path('music/', views.music, name='music'),
-]
--- aislarte_django/aislarte/multilevel_webpage/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from multilevel_webpage import views
-
-urlpatterns = [
-    path('', views.multilevel_webpage, name='multilevel_webpage'),
-]
